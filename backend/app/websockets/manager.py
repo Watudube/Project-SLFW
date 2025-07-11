a752786dea80fe3314c4012ee461e58d8c742f6b
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-11 12:26:47 +1000

    Reduced redunancy, changed variable names

--- backend/app/websockets/manager.py
+++ backend/app/websockets/manager.py
@@ -1,22 +1,19 @@
-# app/websockets/manager.py
-import json
 from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.active: list[WebSocket] = []
+        self.connections: list[WebSocket] = []
 
     async def connect(self, ws: WebSocket):
         await ws.accept()
-        self.active.append(ws)
+        self.connections.append(ws)
 
     def disconnect(self, ws: WebSocket):
-        self.active.remove(ws)
+        self.connections.remove(ws)
 
-    async def send_personal(self, msg: dict, ws: WebSocket):
-        await ws.send_json(msg)
+    async def send(self, message: dict, ws: WebSocket):
+        await ws.send_json(message)
 
-    async def broadcast(self, msg: dict):
-        data = json.dumps(msg)
-        for client in self.active:
-            await client.send_text(data)
+    async def broadcast(self, message: dict):
+        for connection in self.connections:
+            await self.send(message, connection)
