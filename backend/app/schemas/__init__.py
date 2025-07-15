2840797212bb89bd13f3fe028bf07ab3aa05cc63
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-15 20:19:07 +1000

    Added init for easier import

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .core.base_schema import *
+from .core.gameboard_schema import *
+from .core.level_schema import *
+from .core.tile_schema import *
+from .core.entity_schema import *
+from .core.actor_schema import *
+from .humans.human_schema import *
