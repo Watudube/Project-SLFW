1c98b2c8636a2f4c7d5507eb4fc5ed5ab102a212
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-17 21:56:57 +1000

    Updated schemas to match updated models

--- backend/app/schemas/core/actor_schema.py
+++ backend/app/schemas/core/actor_schema.py
@@ -5,14 +5,20 @@ class ActorOut(EntityOut):
     health:           int
     speed:            int
     perception_range: int
+    hunger:   int
+    strength: int
 
 class ActorIn(EntityIn):
     health:           int
     speed:            int
     perception_range: int
+    hunger:   int
+    strength: int
 
 
 class ActorUpdate(EntityUpdate):
     health: Optional[int]
     speed: Optional[int]
     perception_range: Optional[int]
+    hunger: Optional[int]
+    strength: Optional[int]
--- backend/app/schemas/humans/human_schema.py
+++ backend/app/schemas/humans/human_schema.py
@@ -1,20 +1,11 @@
 from ..core.actor_schema import ActorOut, ActorIn, ActorUpdate
-from typing import Literal, Optional
+from typing import Literal
 
 class HumanOut(ActorOut):
     type: Literal["human"]
-    name: Optional[str]
-    hunger:   int
-    strength: int
-
 
 class HumanIn(ActorIn):
-    name: Optional[str]
-    hunger:   int
-    strength: int
-
+    pass
 
 class HumanUpdate(ActorUpdate):
-    name: Optional[str]
-    hunger: Optional[int]
-    strength: Optional[int]
+    pass
