9f726fe270430324a20e9e77a89861f4c861c3af
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-18 14:59:55 +1000

    Users are now distinct from entities, they exist without gameboard

---
+++ backend/app/models/core/user.py
@@ -0,0 +1,16 @@
+from .base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    username = Column(String, primary_key=True, unique=True)
+    password = Column(String, nullable=False)
+
+    player = relationship(
+        "app.models.humans.player.Player",
+        back_populates="user",
+        uselist=False,
+        cascade="all, delete-orphan"
+    )
