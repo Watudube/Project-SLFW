894c505f7d1a9e0449d51993b9ae2795545e741a
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-18 15:02:12 +1000

    Player connected 1:1 to user. Player is users playable entity

---
+++ backend/app/models/humans/player.py
@@ -0,0 +1,19 @@
+from .human import Human
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, CheckConstraint
+from sqlalchemy.orm import relationship
+
+class Player(Human):
+    __tablename__ = "players"
+    __table_args__ = (
+        CheckConstraint("id > 0", name = "check_id_positive"),
+    )
+    __mapper_args__ = {"polymorphic_identity": "player"}
+
+    id = Column(Integer, ForeignKey("humans.id"), primary_key=True)
+    username = Column(String, ForeignKey("users.username"), nullable=False)
+
+    user = relationship(
+        "app.models.core.user.User",
+        back_populates="player",
+        uselist=False
+    )
