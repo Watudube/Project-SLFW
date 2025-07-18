24770e54634c06bbf5927846a9d93b97ddfc0c40
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-18 15:12:22 +1000

    Tokens now issued to username instead of id

--- backend/app/apis/user.py
+++ backend/app/apis/user.py
@@ -1,6 +1,6 @@
 from app.db.session import get_session
-from app.schemas.humans.user_schema import AuthResponse, UserIn
-from app.services.humans.user_service import UserService
+from app.schemas.core.user_schema import AuthResponse, UserIn
+from app.services.core.user_service import UserService
 from app.websockets.manager import ConnectionManager
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
@@ -17,15 +17,10 @@ def create_account(user_in: UserIn, db: Session = Depends(get_session)):
             status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists"
         )
 
-    user_in = user_in.model_dump()  # Converts Pydantic model to dict.
-    user_in["type"] = "user"
-
-    # Kevin Changed this: Access username as a key as it is now a dict.
-    user_in["label"] = [user_in["username"]]
-
+    user_in = user_in.model_dump()
     user = service.create_user(user_in)
-    token = manager.issue_token(user.id)
-    return {"user": user, "token": token}
+    token = manager.issue_token(user.username)
+    return {"token": token}
 
 
 @router.post("/login", response_model=AuthResponse)
@@ -37,5 +32,5 @@ def login(user_in: UserIn, db: Session = Depends(get_session)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect username or password",
         )
-    token = manager.issue_token(user.id)
-    return {"user": user, "token": token}
+    token = manager.issue_token(user.username)
+    return {"token": token}
