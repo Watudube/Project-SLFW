82ddf126797d712d0311f89d154be19439620847
Hai Peng Kevin Goh <kevingoh_2002@hotmail.com>
2025-07-18 15:34:21 +1000

    Update endpointURLs.js
    Changed frontend's reference to backend endpoint to localhost. For local development - as default.

--- frontend/src/services/endpointURLs.js
+++ frontend/src/services/endpointURLs.js
@@ -1,8 +1,8 @@
 // NOTE TO DEVS: Change I.P.s as required!!! Make sure to comment out/in the correct line for your environment.
 
 // IMPORTANT: Change the string to the local network backend host I.P.:
-const domainURL = "10.89.100.213"; // This is for local network development.
-//const domainURL = "localhost"; // This is for localhost development.
+// const domainURL = "10.89.100.213"; // This is for local network development.
+const domainURL = "localhost"; // This is for localhost development.
 
 // Endpoint templates:
 export const WEBSOCKET_BASEURL = `ws://${domainURL}:8000/ws/game`;
