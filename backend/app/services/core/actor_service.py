8946f68253aef0465fe840b19ded107130fa837f
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-26 09:03:20 +1000

    Fixed perception after moving subsection functionality to level service

--- backend/app/services/core/actor_service.py
+++ backend/app/services/core/actor_service.py
@@ -3,8 +3,9 @@ from typing import TypeVar, Generic
 from .entity_service import EntityService
 from app.models.core.actor import Actor
 from app.repositories.core.actor_repo import ActorRepository
-from app.models.core.tile import Tile
+from app.models.core.level import Level
 from .tile_service import TileService
+from .level_service import LevelService
 
 M = TypeVar("M", bound=Actor)
 R = TypeVar("R", bound=ActorRepository)
@@ -13,19 +14,19 @@ class ActorService(EntityService[M, R], Generic[M, R]):
     def __init__(self, db: Session, model: type[M], repo_class: type[R]):
         super().__init__(db, model, repo_class)
 
-    def get_perception(self, actor_id: int) -> list[Tile]:
+    def get_perception(self, actor: Actor) -> Level:
         """
         Provides all the tiles and entities that an actor can see within their
         range of vision
         Params:
-            - actor_id: id of the actor whose perception we are checking
+            - actor: Actor model instance
         returns:
             - list[Tile]: A list of tiles (containing entities) within range of the actor
         """
-        actor = self.get(actor_id)
         tile_service = TileService(self.db)
         tile = tile_service.get_tile(actor.tile_id)
-        return tile_service.get_subsection(
+        level_service = LevelService(self.db)
+        return level_service.get_subsection(
             tile.level_id,
             tile.x_coord,
             tile.y_coord,
