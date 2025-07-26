7e87bbbb1ce8f0809e16c91381b46a8741ff6377
Hai Peng Kevin Goh <kevingoh_2002@hotmail.com>
2025-07-26 10:51:31 +1000

    Update sceneKeys.js
    Fixed typo. Inconsequential.

--- frontend/src/phaser/sceneKeys.js
+++ frontend/src/phaser/sceneKeys.js
@@ -1,5 +1,5 @@
 /**
- * Thiis object contains the keys for the different scenes in the Phaser game.
+ * This object contains the keys for the different scenes in the Phaser game.
  */
 export const SCENE_KEYS = Object.freeze({
   OVERWORLD_SCENE: "OVERWORLD_SCENE",
