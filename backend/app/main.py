c2bec1be47d6b5ad5ca32651e1116ec82a5541b1
Marshall-Bullis <marshall.bullis@outlook.com>
2025-07-27 16:49:40 +1000

    Fixing hosts

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,8 +25,6 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://192.168.1.115:5173",
-        "http://192.168.1.142:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
